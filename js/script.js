f09d8bd4cde724385957b349b590a4cc4f1640cf
Matheus <msgoncalves@inf.ufpel.edu.br>
2024-01-13 15:10:39 -0300

    Update modal styles and add script for modal functionality

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// Seleção de elementos
+const openModalButton = document.querySelector("#open-modal");
+const closeModalButton = document.querySelector("#close-modal");
+const modal = document.querySelector("#modal");
+const fade = document.querySelector("#fade");
+
+const toggleModal = () => {
+    [modal, fade].forEach((el) => el.classList.toggle("hide"));
+}
+// Eventos
+[openModalButton, closeModalButton, fade].forEach((el) => {
+    el.addEventListener("click", () => toggleModal());
+})
